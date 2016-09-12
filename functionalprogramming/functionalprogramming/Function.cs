5c47d00053d99a320dc130fd82f76b78285624e9
ArsalonAmini <aminihajibasa@gmail.com>
2016-09-12 06:20:34 -0500

    functional production code

---
+++ functionalprogramming/functionalprogramming/Function.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functionalprogramming
+{
+    class Function
+    {
+
+        public Function()
+        {
+
+        }
+
+        public void GetAverage()
+        {
+            string grades = "1,7,15,35,24,18,1992";
+            var numbers = grades.Split(',').Select(Int32.Parse).ToList();
+            numbers.Sort();
+            double result = (from x in numbers where x > numbers[2] select x).Average();
+            Console.WriteLine(result);
+        }
+
+        public void GetNameSplit()
+        {
+            string name = "Llewellyn";
+            name = string.Concat(name.ToLower().OrderBy(z => z));
+            var letterCount = name.GroupBy(c => c).Select(collect => new { Char = collect.Key, Count = collect.Count() });
+            foreach (var x in letterCount) { Console.Write("{0}{1}", x.Char, x.Count); }
+        }
+    }
+}
+
---
+++ functionalprogramming/functionalprogramming/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace functionalprogramming
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Function function = new Function();
+            function.GetAverage();
+            function.GetNameSplit();
+            Console.ReadLine();
+
+        }
+
+
+
+
+    }
+
+}
